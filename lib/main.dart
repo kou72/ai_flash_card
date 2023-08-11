dc07b749016fbfbc20c2608617d67a7d59e60035
kou72 <genpokopokopoko@gmail.com>
2023-08-11 12:34:33 +0900

    .gitignoreファイルにpubspec.lockを追加、pubspec.yamlにfile_pickerパッケージを追加
    .gitignore
    - pubspec.lockを無視するように追加
    
    pubspec.yaml
    - file_pickerパッケージを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() => runApp(const MyApp());
 
@@ -19,6 +20,21 @@ class MyApp extends StatelessWidget {
 class StartScreen extends StatelessWidget {
   const StartScreen({Key? key}) : super(key: key);
 
+  Future<void> _pickPDF() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+
+    if (result != null) {
+      PlatformFile file = result.files.first;
+      print(file.name); // PDFファイルの名前を表示
+      // ここでファイルをアップロードするロジックを実装する
+    } else {
+      // ユーザーがファイル選択をキャンセルした場合の処理
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,13 +50,8 @@ class StartScreen extends StatelessWidget {
             ),
             const SizedBox(height: 20.0),
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const UploadAndProcessScreen()),
-                );
-              },
+              onPressed: _pickPDF,
+              // onPressed: () => {},
               child: const Text('PDFをアップロード'),
             ),
           ],
@@ -50,131 +61,131 @@ class StartScreen extends StatelessWidget {
   }
 }
 
-class UploadAndProcessScreen extends StatefulWidget {
-  const UploadAndProcessScreen({Key? key}) : super(key: key);
-  @override
-  UploadAndProcessScreenState createState() => UploadAndProcessScreenState();
-}
+// class UploadAndProcessScreen extends StatefulWidget {
+//   const UploadAndProcessScreen({Key? key}) : super(key: key);
+//   @override
+//   UploadAndProcessScreenState createState() => UploadAndProcessScreenState();
+// }
 
-class UploadAndProcessScreenState extends State<UploadAndProcessScreen> {
-  bool _isUploading = false;
-  double _progressValue = 0.0;
+// class UploadAndProcessScreenState extends State<UploadAndProcessScreen> {
+//   bool _isUploading = false;
+//   double _progressValue = 0.0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('アップロード&処理'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                // PDFアップロードのロジックをここに実装
-                // 例: uploadPDF();
-              },
-              child: const Text('PDFをアップロード'),
-            ),
-            const SizedBox(height: 20),
-            if (_isUploading) ...[
-              const Text('処理中...'),
-              const SizedBox(height: 10),
-              LinearProgressIndicator(value: _progressValue),
-            ],
-          ],
-        ),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('アップロード&処理'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             ElevatedButton(
+//               onPressed: () {
+//                 // PDFアップロードのロジックをここに実装
+//                 // 例: uploadPDF();
+//               },
+//               child: const Text('PDFをアップロード'),
+//             ),
+//             const SizedBox(height: 20),
+//             if (_isUploading) ...[
+//               const Text('処理中...'),
+//               const SizedBox(height: 10),
+//               LinearProgressIndicator(value: _progressValue),
+//             ],
+//           ],
+//         ),
+//       ),
+//     );
+//   }
 
-  // この関数はPDFのアップロードとテキスト抽出のロジックを示すダミーです。
-  // 実際のロジックを実装する際には、適切な方法で更新する必要があります。
-  Future<void> uploadPDF() async {
-    setState(() {
-      _isUploading = true;
-      _progressValue = 0.1;
-    });
+//   // この関数はPDFのアップロードとテキスト抽出のロジックを示すダミーです。
+//   // 実際のロジックを実装する際には、適切な方法で更新する必要があります。
+//   Future<void> uploadPDF() async {
+//     setState(() {
+//       _isUploading = true;
+//       _progressValue = 0.1;
+//     });
 
-    // PDFのアップロードとテキスト抽出のロジックをここに実装
+//     // PDFのアップロードとテキスト抽出のロジックをここに実装
 
-    // ダミーの暗記カードリスト
-    List<Flashcard> flashcards = [
-      Flashcard(question: "質問1", answer: "答え1"),
-      Flashcard(question: "質問2", answer: "答え2"),
-      // ... 他の暗記カード
-    ];
+//     // ダミーの暗記カードリスト
+//     List<Flashcard> flashcards = [
+//       Flashcard(question: "質問1", answer: "答え1"),
+//       Flashcard(question: "質問2", answer: "答え2"),
+//       // ... 他の暗記カード
+//     ];
 
-    setState(() {
-      _progressValue = 1.0;
-      _isUploading = false;
-    });
-    // ResultScreenへの遷移
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => ResultScreen(flashcards: flashcards),
-      ),
-    );
-  }
-}
+//     setState(() {
+//       _progressValue = 1.0;
+//       _isUploading = false;
+//     });
+//     // ResultScreenへの遷移
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => ResultScreen(flashcards: flashcards),
+//       ),
+//     );
+//   }
+// }
 
-class ResultScreen extends StatefulWidget {
-  final List<Flashcard> flashcards;
+// class ResultScreen extends StatefulWidget {
+//   final List<Flashcard> flashcards;
 
-  const ResultScreen({super.key, required this.flashcards});
+//   const ResultScreen({super.key, required this.flashcards});
 
-  @override
-  ResultScreenState createState() => ResultScreenState();
-}
+//   @override
+//   ResultScreenState createState() => ResultScreenState();
+// }
 
-class ResultScreenState extends State<ResultScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('暗記カードの結果'),
-      ),
-      body: ListView.builder(
-        itemCount: widget.flashcards.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(widget.flashcards[index].question),
-            onTap: () {
-              // タップしたときの詳細表示ロジック
-              _showDetail(widget.flashcards[index]);
-            },
-          );
-        },
-      ),
-    );
-  }
+// class ResultScreenState extends State<ResultScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('暗記カードの結果'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: widget.flashcards.length,
+//         itemBuilder: (context, index) {
+//           return ListTile(
+//             title: Text(widget.flashcards[index].question),
+//             onTap: () {
+//               // タップしたときの詳細表示ロジック
+//               _showDetail(widget.flashcards[index]);
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
 
-  void _showDetail(Flashcard flashcard) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(flashcard.question),
-          content: Text(flashcard.answer),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('閉じる'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
+//   void _showDetail(Flashcard flashcard) {
+//     showDialog(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: Text(flashcard.question),
+//           content: Text(flashcard.answer),
+//           actions: <Widget>[
+//             TextButton(
+//               child: const Text('閉じる'),
+//               onPressed: () {
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
 
-class Flashcard {
-  final String question;
-  final String answer;
+// class Flashcard {
+//   final String question;
+//   final String answer;
 
-  Flashcard({required this.question, required this.answer});
-}
+//   Flashcard({required this.question, required this.answer});
+// }
