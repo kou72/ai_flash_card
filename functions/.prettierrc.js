13e99df25c291ba20b716920031fc91bb6751e2c
kou72 <genpokopokopoko@gmail.com>
2023-08-12 23:55:33 +0900

    functions 作成

---
+++ functions/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: false, // ダブルクォートを使用
+  bracketSpacing: false, // { foo: bar } のようにブレースの内側にスペースを入れる
+  semi: true, // 文末にセミコロンを使用
+  useTabs: false, // タブの代わりにスペースを使用
+  tabWidth: 2, // インデントは2スペース
+  printWidth: 80, // 1行の最大の文字数
+  arrowarrowParens: "always", // 矢印関数の引数に括弧を使用する
+  quoteProps: "consistent", // オブジェクトのプロパティには必要に応じて引用符を使用する
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,91 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import vision from "@google-cloud/vision";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const storage = admin.storage();
+
+// 実行時間のyyyyMMddhhmmssを取得
+const getDate = () => {
+  const date = new Date();
+  const formattedDate = `${date.getFullYear()}${String(
+    date.getMonth() + 1
+  ).padStart(2, "0")}${String(date.getDate()).padStart(2, "0")}${String(
+    date.getHours()
+  ).padStart(2, "0")}${String(date.getMinutes()).padStart(2, "0")}${String(
+    date.getSeconds()
+  ).padStart(2, "0")}`;
+  return formattedDate;
+};
+
+const getFileContents = async (DestinationFolder: string) => {
+  const bucket = storage.bucket();
+  const [files] = await bucket.getFiles({prefix: DestinationFolder});
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [texts] = files.map(async (file: any) => {
+    const json = bucket.file(file.name);
+    const contents = await json.download();
+    const jsonString = contents.toString();
+    const jsonData = JSON.parse(jsonString);
+    return jsonData.responses[0].fullTextAnnotation.text;
+  });
+
+  return texts;
+};
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const documentTextDetection = onRequest(async (request, response) => {
+  const client = new vision.ImageAnnotatorClient();
+
+  const bucketName = "flash-pdf-card.appspot.com";
+  const fileName = "test.pdf";
+  const gcsSourceUri = `gs://${bucketName}/${fileName}`;
+
+  const baseFileName = fileName.replace(".pdf", "");
+  const date = getDate();
+  const DestinationFolder = `${baseFileName}${date}/`;
+  const gcsDestinationUri = `gs://${bucketName}/${DestinationFolder}`;
+
+  const inputConfig = {
+    mimeType: "application/pdf",
+    gcsSource: {
+      uri: gcsSourceUri,
+    },
+  };
+  const outputConfig = {
+    gcsDestination: {
+      uri: gcsDestinationUri,
+    },
+  };
+  const features = [{type: "DOCUMENT_TEXT_DETECTION"}];
+  const config = {
+    requests: [
+      {
+        inputConfig: inputConfig,
+        features: features,
+        outputConfig: outputConfig,
+      },
+    ],
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [operation]: any = await client.asyncBatchAnnotateFiles(config as any);
+  await operation.promise();
+
+  const texts = await getFileContents(DestinationFolder);
+  response.send(texts);
+
+  const bucket = storage.bucket();
+  const [files] = await bucket.getFiles();
+  logger.info("Files:" + files[0].name, {structuredData: true});
+
+  response.send("Document Text Detection");
+});
