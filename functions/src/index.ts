de4df12bf3f36fb4128c50396f26ccbfaeb8a8f4
kou72 <genpokopokopoko@gmail.com>
2023-08-16 23:49:25 +0900

    index.tsファイルの変更内容を解析します。
    functions/src/index.ts
    - import文の追加
    - generateFlashCardQuestions関数の追加
    - generateFlashCardQuestions関数内でのPOSTリクエストの処理追加
    - extractTextFromJson関数の削除
    - helloWorld関数の削除
    - documentTextDetection関数の削除
    
    functions/src/index.ts
    - visionモジュールをimport
    - generateFlashCardQuestions関数を追加
      - POSTリクエストがあった場合に、ファイルのアップロード、変換、テキスト抽出、質問生成を行い、結果を返す
    - extractTextFromJson関数の削除
    - helloWorld関数の削除
    - documentTextDetection関数の削除

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import * as os from "os";
-// import * as path from "path";
-// import * as fs from "fs";
 import {onRequest, Request} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-import vision from "@google-cloud/vision";
 import * as admin from "firebase-admin";
 import * as busboy from "busboy";
+import vision from "@google-cloud/vision";
 import {getDate} from "./utils";
 import {generateQuestionsFromChatGPT} from "./openai";
 
@@ -15,6 +12,30 @@ admin.initializeApp();
 const storage = admin.storage();
 const bucket = storage.bucket();
 const bucketName = "flash-pdf-card.appspot.com";
+
+export const generateFlashCardQuestions = onRequest(
+  {timeoutSeconds: 300, cors: true},
+  async (req, res) => {
+    if (req.method !== "POST") {
+      res.status(405).end();
+      return;
+    }
+
+    try {
+      const date = getDate();
+      const sourcePath = await fileUpload(req, date);
+      // dest用pathをsourcePathから生成（upload/0000-text.pdf → destination/0000-text）
+      const destFolder =
+        "dest/" + sourcePath.split("/")[1].replace(/\.[^/.]+$/, "") + "/";
+      await convertPdfToTextJson(sourcePath, destFolder);
+      const texts = await extractTextFromJson(destFolder);
+      const questions = await generateQuestionsFromChatGPT(texts);
+      res.status(200).send(questions);
+    } catch (error: any) {
+      res.status(500).send(error.message);
+    }
+  }
+);
 
 const fileUpload = (req: Request, date: string): Promise<string> =>
   new Promise((resolve, reject) => {
@@ -83,119 +104,3 @@ const extractTextFromJson = async (DestinationFolder: string) => {
     logger.error(error.message, {structuredData: true});
   }
 };
-
-export const helloWorld = onRequest(
-  {timeoutSeconds: 300, cors: true},
-  async (req, res) => {
-    if (req.method !== "POST") {
-      res.status(405).end();
-      return;
-    }
-
-    try {
-      const date = getDate();
-      const sourcePath = await fileUpload(req, date);
-      // dest用pathをsourcePathから生成（upload/0000-text.pdf → destination/0000-text）
-      const destFolder =
-        "dest/" + sourcePath.split("/")[1].replace(/\.[^/.]+$/, "") + "/";
-      await convertPdfToTextJson(sourcePath, destFolder);
-      const texts = await extractTextFromJson(destFolder);
-      const questions = await generateQuestionsFromChatGPT(texts);
-      res.status(200).send(questions);
-    } catch (error: any) {
-      res.status(500).send(error.message);
-    }
-  }
-);
-
-export const documentTextDetection = onRequest(
-  {
-    timeoutSeconds: 300,
-    cors: true,
-  },
-  async (req, res) => {
-    // CORS
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      res.header("Access-Control-Allow-Methods", "POST");
-      res.status(200).end();
-      return;
-    }
-
-    // POST以外の場合は405エラー
-    if (req.method !== "POST") {
-      res.status(405).end();
-      return;
-    }
-
-    logger.info("req.body: " + req.body);
-    logger.info("req.body.filename: " + req.body.filename);
-
-    const bb = busboy({headers: req.headers});
-
-    bb.on(
-      "file",
-      (
-        fieldname: any,
-        file: any,
-        filename: any,
-        encoding: any,
-        mimetype: any
-      ) => {
-        logger.info("fieldname: " + fieldname);
-        logger.info("file: " + file);
-        logger.info("filename: " + filename);
-        logger.info("encoding: " + encoding);
-        logger.info("mimetype: " + mimetype);
-      }
-    );
-
-    // const tmpdir = os.tmpdir();
-
-    const client = new vision.ImageAnnotatorClient();
-
-    // const fileName = "test.pdf";
-    const fileName = "l2pt.pdf";
-    const gcsSourceUri = `gs://${bucketName}/${fileName}`;
-
-    const baseFileName = fileName.replace(".pdf", "");
-    const date = getDate();
-    const DestinationFolder = `${baseFileName}${date}/`;
-    const gcsDestinationUri = `gs://${bucketName}/${DestinationFolder}`;
-
-    const inputConfig = {
-      mimeType: "application/pdf",
-      gcsSource: {
-        uri: gcsSourceUri,
-      },
-    };
-    const outputConfig = {
-      gcsDestination: {
-        uri: gcsDestinationUri,
-      },
-    };
-    const features = [{type: "DOCUMENT_TEXT_DETECTION"}];
-    const config = {
-      requests: [
-        {
-          inputConfig: inputConfig,
-          features: features,
-          outputConfig: outputConfig,
-        },
-      ],
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const [operation]: any = await client.asyncBatchAnnotateFiles(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      config as any
-    );
-    await operation.promise();
-
-    // const texts = await getFileContents(bucket, DestinationFolder);
-    // const chatGptRes = await requestChatGPT(texts);
-    // await bucket.deleteFiles({prefix: DestinationFolder}); // ファイルを削除する場合
-    // res.send(chatGptRes);
-  }
-);
