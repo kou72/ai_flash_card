c54b300dc4d8cca05e3ad9d77cbf757a06e8e18e
kou72 <genpokopokopoko@gmail.com>
2023-08-19 02:29:55 +0900

    resultscreen.dartファイルに以下の変更が加えられている。
    - cardsDataというリストを追加し、質問と答え、フリップ状態を持つオブジェクトを要素として持つようにした。
    - bodyの部分をListViewからListView.builderに変更し、cardsDataの要素数分の_flashCardウィジェットを作成するようにした。
    - _flashCardウィジェットを作成し、タップされた場合にフリップ状態を切り替えるようにした。
    
    lib/resultscreen.dart
    - cardsDataリストを追加し、質問と答え、フリップ状態を持つオブジェクトを要素として持つようにした。
    - bodyの部分をListViewからListView.builderに変更し、cardsDataの要素数分の_flashCardウィジェットを作成するようにした。
    - _flashCardウィジェットを作成し、タップされた場合にフリップ状態を切り替えるようにした。

--- lib/resultscreen.dart
+++ lib/resultscreen.dart
@@ -7,18 +7,58 @@ class ResultScreen extends StatefulWidget {
 }
 
 class ResultScreenState extends State<ResultScreen> {
+  final cardsData = [
+    {
+      'question': '質問1',
+      'answer': '答え1',
+      'isFlipped': true,
+    },
+    {
+      'question': '質問2',
+      'answer': '答え2',
+      'isFlipped': true,
+    },
+    {
+      'question': '質問3',
+      'answer': '答え3',
+      'isFlipped': true,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('暗記カード'),
       ),
-      body: ListView(
-        children: [],
+      body: ListView.builder(
+        itemCount: cardsData.length,
+        itemBuilder: (context, index) {
+          return _flashCard(index, cardsData[index]);
+        },
+      ),
+    );
+  }
+
+  Widget _flashCard(int index, Map<String, dynamic> cardData) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          cardData['isFlipped'] = !cardData['isFlipped'];
+        });
+      },
+      child: Card(
+        child: Center(
+          child: cardData['isFlipped']
+              ? Text(cardData['question'])
+              : Text(cardData['answer']),
+        ),
       ),
     );
   }
 }
+
+
 
 // class ResultScreen extends StatelessWidget {
 //   final String resultText;
