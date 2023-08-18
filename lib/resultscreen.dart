b3eeb65d0b2382bb4c7f14d9172aa5e2f2bffce1
kou72 <genpokopokopoko@gmail.com>
2023-08-19 01:59:24 +0900

    lib/resultscreen.dartファイルとlib/startscreen.dartファイルの変更点を分析します。
    lib/resultscreen.dart
    - ResultScreenクラスをStatefulWidgetに変更し、ResultScreenStateクラスを作成
    - ResultScreenStateクラスで、ResultScreenの状態を管理する
    - AppBarのタイトルを「暗記カードの結果」から「暗記カード」に変更
    - childrenリストを空に変更
    
    lib/startscreen.dart
    - _pickedFileNameが空の場合に表示するSnackBarのコードをコメントアウト
    - _isLoadingをtrueに設定し、httpリクエストを送信するコードをコメントアウト
    - httpリクエストのレスポンスを表示するコードをコメントアウト
    - ResultScreenの引数resultTextをコメントアウトし、ResultScreenのインスタンスを作成するコードをコメントアウト
    
    lib/resultscreen.dart
    - ResultScreenクラスをStatefulWidgetに変更し、ResultScreenStateクラスを作成
    - ResultScreenStateクラスで、ResultScreenの状態を管理する
    - AppBarのタイトルを「暗記カードの結果」から「暗記カード」に変更
    - childrenリストを空に変更
    
    lib/startscreen.dart
    - _pickedFileNameが空の場合に表示するSnackBarのコードをコメントアウト
    - _isLoadingをtrueに設定し、httpリクエストを送信するコードをコメントアウト
    - httpリクエストのレスポンスを表示するコードをコメントアウト
    - ResultScreenの引数resultTextをコメントアウトし、ResultScreenのインスタンスを作成するコードをコメントアウト

--- lib/resultscreen.dart
+++ lib/resultscreen.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 
-class ResultScreen extends StatelessWidget {
-  final String resultText;
-  const ResultScreen({Key? key, required this.resultText}) : super(key: key);
+class ResultScreen extends StatefulWidget {
+  const ResultScreen({Key? key}) : super(key: key);
+  @override
+  ResultScreenState createState() => ResultScreenState();
+}
 
+class ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('暗記カードの結果'),
+        title: const Text('暗記カード'),
       ),
       body: ListView(
-        children: [
-          ListTile(
-            title: Text('質問: ${resultText}'), // 例として、ここでresultTextを表示しています
-            // subtitle: Text('答え: ${resultText}'),
-          ),
-        ],
+        children: [],
       ),
     );
   }
 }
+
+// class ResultScreen extends StatelessWidget {
+//   final String resultText;
+//   const ResultScreen({Key? key, required this.resultText}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('暗記カードの結果'),
+//       ),
+//       body: ListView(
+//         children: [
+//           ListTile(
+//             title: Text('質問: ${resultText}'), // 例として、ここでresultTextを表示しています
+//             // subtitle: Text('答え: ${resultText}'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
