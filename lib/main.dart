80af7164be6cf3e6353edac0c1c3d156d043690f
kou72 <genpokopokopoko@gmail.com>
2023-08-20 03:02:38 +0900

    main.dartファイルのアプリのタイトルを変更
    main.dart
    - タイトルを'Flash PDF Card'に設定
    + タイトルを'Flash PDF Card (Demo)'に変更

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flash PDF Card',
+      title: 'Flash PDF Card (Demo)',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
