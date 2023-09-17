3fc8ca7702d75d33555f6391dbdfc3b46e64a7b8
kou72 <suzukiko7273@gmail.com>
2023-09-18 00:12:39 +0900

    lib/deck.dartファイルにFloatingActionButtonを追加
    lib/deck.dart
    - DeckStateクラスのbuildメソッド内にFloatingActionButtonを追加
    - 2つのFloatingActionButtonを追加し、それぞれのボタンにアイコンとラベルを設定
    - onPressedイベントはまだ実装されていない

--- lib/deck.dart
+++ lib/deck.dart
@@ -26,6 +26,24 @@ class DeckState extends State<Deck> {
       //     return _flashCard(index);
       //   },
       // ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton.extended(
+            heroTag: "ai",
+            icon: const Icon(Icons.smart_toy),
+            label: const Text('画像からカード生成'),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton.extended(
+            heroTag: "card",
+            icon: const Icon(Icons.edit),
+            label: const Text('自分でカードを作成'),
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
