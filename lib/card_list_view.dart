e78a91492956c89f9ae2f91cf6d19cb136d9961f
kou72 <suzukiko7273@gmail.com>
2023-10-01 21:25:22 +0900

    card_list_view.dartファイルの変更内容を分析すると以下のようになります。
    card_list_view.dart
    - CardListViewクラスをHookConsumerWidgetからConsumerStatefulWidgetに変更
    - CardListViewStateクラスを新規作成し、ConsumerState<CardListView>を継承
    - initStateメソッドを追加し、_scrollControllerを初期化して_scrollListenerを追加
    - disposeメソッドを追加し、_scrollControllerを破棄
    - _scrollListenerメソッドを追加し、スクロール位置に応じて_showFabを制御
    - buildメソッド内でcardsStreamを監視し、_asyncCardListメソッドを呼び出す
    - _asyncCardListメソッド内でcardsStreamの状態に応じてカードリストを表示
    - _cardListメソッド内でカードリストを表示するListViewを作成
    - _floatingActionButtonメソッド内でFABを表示するためのウィジェットを作成
    
    以上の変更内容を反映したコミットメッセージは以下のようになります。
    
    card_list_view.dart
    - CardListViewクラスをConsumerStatefulWidgetに変更
    - CardListViewStateクラスを新規作成し、ConsumerState<CardListView>を継承
    - initStateメソッドを追加し、_scrollControllerを初期化して_scrollListenerを追加
    - disposeメソッドを追加し、_scrollControllerを破棄
    - _scrollListenerメソッドを追加し、スクロール位置に応じて_showFabを制御
    - buildメソッド内でcardsStreamを監視し、_asyncCardListメソッドを呼び出す
    - _asyncCardListメソッド内でcardsStreamの状態に応じてカードリストを表示
    - _cardListメソッド内でカードリストを表示するListViewを作成
    - _floatingActionButtonメソッド内でFABを表示するためのウィジェットを作成

--- lib/card_list_view.dart
+++ lib/card_list_view.dart
@@ -4,68 +4,99 @@ import 'package:flash_pdf_card/ai_dialog.dart';
 import 'package:flash_pdf_card/components/gradient_floating_action_button.dart';
 import "riverpod/cards_state.dart";
 
-class CardListView extends HookConsumerWidget {
+class CardListView extends ConsumerStatefulWidget {
   const CardListView({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final cardsDatabase = ref.watch(cardsDatabaseProvider);
-    final cardsStream = ref.watch(cardsStreamProvider);
+  CardListViewState createState() => CardListViewState();
+}
 
+class CardListViewState extends ConsumerState<CardListView> {
+  late ScrollController _scrollController;
+  bool _showFab = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  // カードと重なるためスクロールが一番下に来たらFABを非表示にする
+  void _scrollListener() {
+    double positionRate =
+        _scrollController.offset / _scrollController.position.maxScrollExtent;
+    if (positionRate == 1) {
+      setState(() => _showFab = false);
+    } else {
+      setState(() => _showFab = true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final cardsStream = ref.watch(cardsStreamProvider);
     return Scaffold(
-      // body: ListView.builder(
-      //   itemCount: flashCardData.length,
-      //   itemBuilder: (context, index) {
-      //     return _flashCard(index);
-      //   },
-      // ),
       body: Center(
         child: _asyncCardList(cardsStream),
       ),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GradientFloatingActionButton(
-            onPressed: () {
-              _showAiDialog(context);
-            },
-            iconData: Icons.smart_toy,
-            label: '画像からカード生成',
-            gradientColors: const [Colors.blue, Colors.purple],
-          ),
-          const SizedBox(height: 16),
-          FloatingActionButton.extended(
-            heroTag: 'createCard',
-            icon: const Icon(Icons.edit),
-            label: const Text('自分でカードを作成'),
-            onPressed: () async {
-              await cardsDatabase.insertCard("question", "answer");
-            },
-          ),
-        ],
-      ),
+      floatingActionButton: _floatingActionButton(),
     );
   }
-}
 
-Widget _asyncCardList(AsyncValue cardsStream) {
-  return cardsStream.when(
-    data: (cards) => _cardList(cards),
-    loading: () => const CircularProgressIndicator(),
-    error: (error, stackTrace) => Text('error: $error'),
-  );
-}
+  Widget _asyncCardList(AsyncValue cardsStream) {
+    return cardsStream.when(
+      data: (cards) => _cardList(cards),
+      loading: () => const CircularProgressIndicator(),
+      error: (error, stackTrace) => Text('error: $error'),
+    );
+  }
 
-Widget _cardList(List cards) {
-  return ListView.builder(
-    itemCount: cards.length,
-    itemBuilder: (context, index) {
-      return ListTile(
-        leading: const Icon(Icons.sell),
-        title: Text(cards[index].question),
-        onTap: () {},
-      );
-    },
-  );
+  Widget _cardList(List cards) {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: cards.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.sell),
+          title: Text(cards[index].question),
+          onTap: () {},
+        );
+      },
+    );
+  }
+
+  Widget _floatingActionButton() {
+    final cardsDatabase = ref.watch(cardsDatabaseProvider);
+    if (!_showFab) return const Column();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        GradientFloatingActionButton(
+          onPressed: () {
+            _showAiDialog(context);
+          },
+          iconData: Icons.smart_toy,
+          label: '画像からカード生成',
+          gradientColors: const [Colors.blue, Colors.purple],
+        ),
+        const SizedBox(height: 16),
+        FloatingActionButton.extended(
+          heroTag: 'createCard',
+          icon: const Icon(Icons.edit),
+          label: const Text('自分でカードを作成'),
+          onPressed: () async {
+            await cardsDatabase.insertCard("question", "answer");
+          },
+        ),
+      ],
+    );
+  }
 }
 
 void _showAiDialog(BuildContext context) {
