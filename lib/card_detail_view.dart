be363ef13b3e88990e3f329ea11a0a892ec4530a
kou72 <suzukiko7273@gmail.com>
2023-10-07 14:44:55 +0900

    card_detail_view.dartファイルの修正内容を解析します。
    card_detail_view.dart
    - _question、_answer、_noteという3つの変数を追加
    - _textFildCard()メソッドを_questionCard()メソッドに変更
    - _answerCard()メソッドを追加
    - _noteCard()メソッドを追加
    - _noteCard()メソッド内で、_note変数の値を更新するように変更

--- lib/card_detail_view.dart
+++ lib/card_detail_view.dart
@@ -7,6 +7,9 @@ class CardDetailView extends StatefulWidget {
 }
 
 class CardDetailViewState extends State<CardDetailView> {
+  String _question = '';
+  String _answer = '';
+  String _note = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,10 +19,13 @@ class CardDetailViewState extends State<CardDetailView> {
             children: [
               const SizedBox(height: 12),
               const Text('問題'),
-              _textFildCard(),
+              _questionCard(),
               const SizedBox(height: 24),
               const Text('答え'),
-              _textFildCard(),
+              _answerCard(),
+              const SizedBox(height: 24),
+              const Text('ノート'),
+              _noteCard(),
               const SizedBox(height: 24),
               _saveButton(),
               const SizedBox(height: 12),
@@ -32,7 +38,7 @@ class CardDetailViewState extends State<CardDetailView> {
     );
   }
 
-  Widget _textFildCard() {
+  Widget _questionCard() {
     const cardWidth = 400.0;
     const cardHeight = 200.0;
     return Card(
@@ -53,7 +59,65 @@ class CardDetailViewState extends State<CardDetailView> {
               border: InputBorder.none,
             ),
             onChanged: (String value) {
-              // setState(() => _deckName = value);
+              setState(() => _question = value);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _answerCard() {
+    const cardWidth = 400.0;
+    const cardHeight = 200.0;
+    return Card(
+      margin: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: cardWidth,
+        height: cardHeight,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 8.0,
+            bottom: 8.0,
+            left: 24.0,
+            right: 24.0,
+          ),
+          child: TextField(
+            maxLines: null,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+            ),
+            onChanged: (String value) {
+              setState(() => _answer = value);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _noteCard() {
+    const cardWidth = 400.0;
+    const cardHeight = 200.0;
+    return Card(
+      margin: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: cardWidth,
+        height: cardHeight,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 8.0,
+            bottom: 8.0,
+            left: 24.0,
+            right: 24.0,
+          ),
+          child: TextField(
+            maxLines: null,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+            ),
+            onChanged: (String value) {
+              setState(() => _note = value);
             },
           ),
         ),
