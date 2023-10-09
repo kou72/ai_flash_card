496fbb0162202cb1219433ab67ac33953d526879
kou72 <suzukiko7273@gmail.com>
2023-10-09 18:49:06 +0900

    test.mjs保存

--- functions/src/test.mjs
+++ functions/src/test.mjs
@@ -1,65 +1,68 @@
-import OpenAI from "openai";
-import {OpenAIStream, StreamingTextResponse} from "ai";
-import {Request} from "node-fetch";
+// eslint-disable-next-line quotes, max-len
+const text = `<1/3>問題:情報通信白書で取り上げられている、ICTとデジタル経済の進化の先にあるとされる社会の形態は何か？回答:Society 5.0
+<2/3>問題:情報通信白書で語られている、ICTの新たな潮流として挙げられている3つの要素は何か？回答:デジタル・プラットフォーマー、AI、サイバーセキュリティ
+<3/3>問題:情報通信白書で示されている、ICTがもたらした新たな経済の姿を表す4つの用語は何か？回答:xTech、シェアリングエコノミー、ギグエコノミー、デジタル・プラットフォーマー
+<end>`;
 
-const openai = new OpenAI({
-  apiKey: "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",
-});
-
-const messages = [
-  {
-    role: "user",
-    content: "こんにちは",
-  },
-];
-
-const req = new Request("http://example.com/api/chat", {
-  method: "POST",
-  body: JSON.stringify({messages}),
-  headers: {"Content-Type": "application/json"},
-});
-
-const fetchResponse = async () => {
-  const {messages} = await req.json();
-
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    stream: true,
-    messages,
-  });
-
-  const openAIStream = OpenAIStream;
-  const stream = openAIStream(response);
-  // const textstream = new StreamingTextResponse(stream);
-  // const body = await textstream.body;
-
-  // console.log(res);
-
+const questionsStreamReader = async (questionsStream) => {
+  const newReadableStream = new ReadableStream({});
+  const reader = questionsStream.getReader();
   const textDecoder = new TextDecoder("utf-8");
-
-  // for await (const chunk of stream) {
-  //   const text = textDecoder.decode(chunk);
-  //   console.log(text);
-  // }
-
-  // for await (const chunk of body) {
-  //   const text = textDecoder.decode(chunk);
-  //   console.log(text);
-  // }
-
-  // eslint-disable-next-line no-constant-condition
-
-  const reader = stream.getReader();
+  let questionsString = "";
+  let isRecordingMeta = false;
+  let metaString = "";
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const {done, value} = await reader.read();
     if (done) {
+      if (!metaString.includes("<end>")) {
+        throw new Error("出力が完了しませんでした。");
+      }
       break;
     }
+
     const text = textDecoder.decode(value);
-    console.log(text);
+    questionsString += text;
+
+    if (text === "<") {
+      metaString = "";
+      isRecordingMeta = true;
+    }
+
+    if (isRecordingMeta) metaString += text;
+
+    if (text === ">") {
+      isRecordingMeta = false;
+      if (metaString.includes("<end>")) {
+        console.log("100%");
+      } else {
+        const regex = /^<(\d+)\/(\d+)>$/; // 正規表現パターン
+        const match = metaString.match(regex);
+        const count = parseInt(match[1], 10) - 1;
+        const total = parseInt(match[2], 10);
+        const progress = (count / total) * 100;
+        console.log(progress.toFixed(0), "%");
+      }
+    }
   }
+
+  console.log(questionsString);
+  return questionsString;
 };
 
-fetchResponse();
+const encoder = new TextEncoder();
+const stream = new ReadableStream({
+  async start(controller) {
+    for (let i = 0; i < text.length; i++) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      controller.enqueue(encoder.encode(text[i]));
+    }
+    controller.close();
+  },
+});
+
+questionsStreamReader(stream)
+  .pipeTo()
+  .then((result) => console.log("Final Result:", result))
+  .catch((error) => console.error("Error:", error));
